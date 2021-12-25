e45c00878c25589ab55ebba858849e4b4c2abaf7
Samhita-Veturi <67774716+Samhita-Veturi@users.noreply.github.com>
2021-12-25 15:23:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+Object_Detector = "";
+Input_Status = "";
+Input = "";
 function preload(){}
 function setup(){
     Canvas = createCanvas(500, 400);
     Canvas.center();
+    Video = createCapture(VIDEO);
+    Video.hide();
 }
-function draw(){}
+function draw(){
+    image(Video, 0, 0, 500, 400);
+}
+function Start(){
+    Object_Detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("Status").innerHTML = "Model Status: Detecting Objects";
+    Input = document.getElementById("User").value;
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Input_Status = true;
+}
